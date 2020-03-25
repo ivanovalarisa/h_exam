3544ae0eb6cfabd9b009b75ff71257263bf0259d
Lisinchyk <lisinchyk@gmail.com>
2020-03-26 00:48:06 +0200

    created new object product data

---
+++ js/data.js
@@ -0,0 +1,68 @@
+'use strict';
+
+let dataGoods = {
+    man: [
+        {
+            id: '31',
+            photo: ['1000085095_001_Zoom_5-Optional_Extra_1.jpg', '1000085095_001_Zoom_2-Back.jpg', '1000085095_001_Zoom_3-Full_Length.jpg'],
+            name: 'Eli Jacket Black',
+            brand: 'ARVUST',
+            size: ['S', 'M', 'L', 'XL', '2XL'],
+            color: 'black',
+            description: 'The Eli Jacket by Arvust is a suede bomber jacket with tonal sherpa collar and lining. This design features a zip through front, side slip pockets, woven logo at the chest, elasticised hemline, made from a blended cotton polyester fabrication and comes in a jet black colourway. Wear over black skinny jeans for a nighttime look.',
+            price: '149,95'
+        },
+        {
+            id: '32',
+            photo: ['1000080984_004_Zoom_1-Front.jpg', '1000080984_004_Zoom_2-Back.jpg', '1000080984_004_Zoom_3-Full_Length.jpg'],
+            name: 'P-6 Logo Uprisal Hoodie Gravel Heather Grey',
+            brand: 'patagonia',
+            size: ['S', 'M', 'L', 'XL'],
+            color: 'Grey',
+            description: 'The P-6 Uprisal Crew by Patagonia is a classic crewneck jumper with a bold logo chest. This design features long sleeves, adjustable hood, woven logo, made of a recycled cotton /polyester fabrication and comes in a grey colourway. Pair back with jeans or chinos for a relaxed vibe.',
+            price: '119,95'
+        },
+        {
+            id: '33',
+            photo: ['1000087469_076_Zoom_1-Front.jpg', '1000087469_076_Zoom_2-Back.jpg', '1000087469_076_Zoom_3-Full_Length.jpg'],
+            name: '501 ’93 Pride Shorts Faded Tie Dye Pink',
+            brand: 'patagonia',
+            size: ['S', 'M', 'L', 'XL'],
+            color: 'Grey',
+            description: 'The P-6 Uprisal Crew by Patagonia is a classic crewneck jumper with a bold logo chest. This design features long sleeves, adjustable hood, woven logo, made of a recycled cotton /polyester fabrication and comes in a grey colourway. Pair back with jeans or chinos for a relaxed vibe.',
+            price: '119,95'
+        },
+    ],
+    women: [
+        {
+            id: '10',
+            photo: ['Zoom_1-Front.jpg', 'Zoom_2-Back.jpg', 'Zoom_3-Full_Length.jpg'],
+            name: 'Franki Floral Mini Dress Black',
+            brand: 'DON\'T ASK AMANDA',
+            size: ['XS', 'S', 'M', 'L'],
+            color: 'black',
+            description: 'The Franki Dress by Don\'t Ask Amanda is a floral mini dress in a black and white colourway. This style features a plunging, V-shaped neckline, short puff sleeves with ruffle detail at the hemline as well as an adjustable tie and an A-line skirt. Pair with chunky gold jewellery for a look that\'s right on trend.',
+            price: '69,95'
+        },
+        {
+            id: '12',
+            photo: ['1000084741_010_Zoom_1-Front.jpg', '1000084741_010_Zoom_2-Back.jpg', '1000084741_010_Zoom_3-Full_Length.jpg'],
+            name: 'Nicole Square Neck Linen Dress White',
+            brand: 'ALICE IN THE EVE',
+            size: ['XS', 'S', 'M', 'L', 'XL'],
+            color: 'white',
+            description: 'The Nicole Dress by Alice In The Eve is a lightweight linen dress with a flattering square neckline This mini dress features a side split at the thigh, thick shoulder straps, made from a soft linen fabrication and comes in a white colourway. Keep it fresh on the hot days in this summer ready mini that can be teamed with sandals or sneakers for different looks.',
+            price: '59,95'
+        },
+        {
+            id: '13',
+            photo: ['5000004662_098_Zoom_1-Front.jpg', '5000004662_098_Zoom_2-Back.jpg', '5000004662_098_Zoom_3-Full_Length.jpg'],
+            name: 'Pointer Slash Jeans Karma',
+            brand: 'Ksubi',
+            size: ['23', '24', '25', '26', '27', '28', '29', '30', '31', '32', ],
+            color: 'blue',
+            description: 'The Pointer Jeans by Ksubi are high waisted jeans with slight distressing and slashes at the knees. This design features a relaxed leg which tapers at the hem, cropped ankle, contrasting tobacco stitch, five pocket styling, zip fly with branded button fastening, signature ksubi cross embroidery, t-box detail, made from a rigid cotton denim and comes in a blue colourway with light vintage stonewash.',
+            price: '189,95'
+        },
+    ]
+};
