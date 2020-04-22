2ee95e93000067bbcfa4f6ce090f1e87f4fa09f2
Larysa Ivanova <larisa.n.ivanova.1988@gmail.com>
2020-04-22 23:04:27 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,85 +1,64 @@
-const gulp = require('gulp'); //
-const babel = require('gulp-babel');//
-const uglify = require('gulp-uglify');//
-const concat = require('gulp-concat');//
-const sourcemaps = require('gulp-sourcemaps');
-const sass = require('gulp-sass');//
-const minify = require('gulp-clean-css');
-const watch = require('gulp-watch');
-// const browserSync = require('browser-sync');
-const browserSync = require('browser-sync').create();
+'use strict';
 
-gulp.task('js', function() {
-    gulp.src('./src/js/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            "presets": ["@babel/preset-env"]
-        }))
-        // .pipe(uglify())
-        // .pipe(concat('main.js'))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./build/js/'))
-        // .pipe(browserSync.stream());
-        // .pipe(browserSync.reload({stream: true}))
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    uglify = require('gulp-uglify'),
+    concat = require('gulp-concat'),
+    babel = require('gulp-babel'),
+    rename = require('gulp-rename'),//
+    del = require('del');//
+
+gulp.task('del', function() {
+    return del('build/');
 });
 
-gulp.task('js-product-build', function() {
-    gulp.src('./src/js/*.js')
+gulp.task('sass', function() {
+    return gulp.src('src/assets/scss/**/*.scss')
+        .pipe(sass({outputStyle: 'compressed'}))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('build/assets/css/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('html', function() {
+    return gulp.src('src/*.html')
+        .pipe(gulp.dest('build/'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('script', function() {
+    return gulp.src('src/assets/js/**/*.js')
         .pipe(babel({
-            "presets": ["@babel/preset-env"]
+            presets: ['@babel/preset-env']
         }))
         .pipe(uglify())
-        .pipe(concat('main.js'))
-        .pipe(gulp.dest('./build/js/'))
+        .pipe(gulp.dest('build/assets/js/'))
         .pipe(browserSync.stream());
-    // .pipe(browserSync.reload({stream: true}))
 });
 
-gulp.task('sass', function () {
-    return gulp.src('./src/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass())
-        .pipe(minify())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./build/css/'))
-        // .pipe(browserSync.stream());
-    // .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('img', function () {
-    return gulp.src('./src/img/**/*.*')
-        .pipe(gulp.dest('./build/img/'))
+gulp.task('copyOther', function() {
+    return gulp.src('src/*.json')
+        .pipe(gulp.dest('build/'));
 });
 
 gulp.task('libs', function() {
-    return gulp.src('src/vendors/**/*.*')
-        .pipe(gulp.dest('build/vendors/'));
+    return gulp.src('src/assets/libs/**/*.*')
+        .pipe(gulp.dest('build/assets/libs'));
 });
 
-gulp.task('html', function () {
-    return gulp.src('./src/*.html')
-        .pipe(gulp.dest('build/'))
-        .pipe(browserSync.stream());
-        // .pipe(browserSync.reload({stream: true}))
-});
-
-gulp.task('watch', function () {
-    watch('./src/scss/**/*.scss', gulp.parallel('sass'));
-    watch('./src/*.html', gulp.parallel('html'));
-    watch('./src/js/*.js', gulp.parallel('js'))
-});
-
-gulp.task('brSync', () => {
+gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
-            baseDir: 'build/' // здесь указываем корневую папку для локального сервера
-        },
-        // notify: false      // уведомления (false - отключение уведомлений)
+            baseDir: "build/"
+        }
     });
 });
 
-// gulp.task('product-build', gulp.parallel('js-product-build', 'sass', 'img', 'libs', 'html', 'watch'));
-// gulp.task('product-build', gulp.parallel('js-product-build', 'sass', 'img', 'brSync', 'watch'));
+gulp.task('watch', function() {
+    gulp.watch('src/assets/scss/**/*.scss', gulp.parallel('sass'));
+    gulp.watch('src/assets/js/**/*.js*', gulp.parallel('script'));
+    gulp.watch('src/*.html', gulp.parallel('html'));
+});
 
-gulp.task('default', gulp.parallel('js', 'sass', 'img', 'libs', 'html', 'watch'));
-// gulp.task('default', gulp.parallel('js', 'sass', 'img', 'brSync', 'watch'));
+gulp.task('default', gulp.series('del', gulp.parallel('sass', 'script', 'html', 'libs', 'copyOther', 'browser-sync', 'watch')));
